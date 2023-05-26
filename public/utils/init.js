b8740eff03de395f22450c5eeab7ae39e51ceef8
Oscar Constantino <12437469+OscarConstantino@users.noreply.github.com>
2023-05-26 02:45:57 +0000

    Adding the Firebase AppCheck SDK

--- public/utils/init.js
+++ public/utils/init.js
@@ -6,3 +6,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+/*
+const appCheck = firebase.appCheck();
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+ 
+appCheck.activate(
+    '6Lca0T4mAAAAACY4effbs_fjKenhHZqwEB0K23al',
+
+   // Optional argument. If true, the SDK automatically refreshes App Check
+   // tokens as needed.
+   true); 
+*/
